eac6b6e0efd495162069fb4654b6aa07e532c74c
chad <chadmcintirecloud@gmail.com>
2024-04-09 18:07:10 -0600

    Need to fix the libs, first commit because installs are done

---
+++ tianshou/trainer/__init__.py
@@ -0,0 +1,19 @@
+"""Trainer package."""
+
+from tianshou.trainer.base import (
+    BaseTrainer,
+    OfflineTrainer,
+    OffpolicyTrainer,
+    OnpolicyTrainer,
+)
+
+from tianshou.trainer.utils import gather_info, test_episode
+
+__all__ = [
+    "BaseTrainer",
+    "OffpolicyTrainer",
+    "OnpolicyTrainer",
+    "OfflineTrainer",
+    "test_episode",
+    "gather_info",
+]
